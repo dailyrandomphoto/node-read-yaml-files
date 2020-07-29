be3dec04ef406b0bc1b38184c5ba1a9d23632093
dailyrandomphoto <dailyrandomphoto@gmail.com>
2020-07-29 16:35:03 +0800

    Apply Prettier code style

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const read = require('node-read-yaml');
 require('array.prototype.flat').shim();
 
 function readFiles(path, options) {
-  options = {recursive: true, depth: 1, ...options};
-  const options2yaml = {...options};
+  options = { recursive: true, depth: 1, ...options };
+  const options2yaml = { ...options };
   delete options2yaml.ignores;
   delete options2yaml.flatten;
   delete options2yaml.depth;
   delete options2yaml.recursive;
 
-  const readYaml = options.withFile ?
-    file => read(file, options2yaml).then(data => ({file, data})) :
-    file => read(file, options2yaml);
+  const readYaml = options.withFile
+    ? (file) => read(file, options2yaml).then((data) => ({ file, data }))
+    : (file) => read(file, options2yaml);
 
   return recursive(path, options.ignores)
-    .then(files => files.sort())
-    .then(files => Promise.all(files.map(readYaml)))
-    .then(arr => options.flatten ? arr.flat(options.depth) : arr);
+    .then((files) => files.sort())
+    .then((files) => Promise.all(files.map((x) => readYaml(x))))
+    .then((array) => (options.flatten ? array.flat(options.depth) : array));
 }
 
 module.exports = readFiles;
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {expect} = require('chai');
+const { expect } = require('chai');
 const readFiles = require('node-read-yaml-files');
 
 describe('node-read-yaml-files', () => {
@@ -9,56 +9,56 @@ describe('node-read-yaml-files', () => {
   });
 
   it('should return an array', () => {
-    return readFiles('./yaml-samples/simple')
-      .then(result => {
-        expect(result).to.be.an('array').to.have.lengthOf(2);
-        expect(result).to.be.eql([
-          {
-            bar: 'foo'
-          },
-          {
-            foo: 'bar'
-          }
-        ]);
-      });
+    return readFiles('./yaml-samples/simple').then((result) => {
+      expect(result).to.be.an('array').to.have.lengthOf(2);
+      expect(result).to.be.eql([
+        {
+          bar: 'foo'
+        },
+        {
+          foo: 'bar'
+        }
+      ]);
+    });
   });
 
   it('should throw an error when read non-YAML files', () => {
     return readFiles('./yaml-samples/yaml-and-others')
       .then(() => expect.fail('should not come to here'))
-      .catch(error => {
+      .catch((error) => {
         expect(error.name).not.to.be.eql('AssertionError');
         expect(error.name).to.be.eql('YAMLException');
       });
   });
 
   it('should ignores some files', () => {
-    return readFiles('./yaml-samples/yaml-and-others', {ignores: ['*.js']})
-      .then(result => {
-        expect(result).to.be.an('array').to.have.lengthOf(2);
-        expect(result).to.be.eql([
-          {
-            bar: 'foo'
-          },
-          {
-            foo: 'bar'
-          }
-        ]);
-      });
+    return readFiles('./yaml-samples/yaml-and-others', {
+      ignores: ['*.js']
+    }).then((result) => {
+      expect(result).to.be.an('array').to.have.lengthOf(2);
+      expect(result).to.be.eql([
+        {
+          bar: 'foo'
+        },
+        {
+          foo: 'bar'
+        }
+      ]);
+    });
   });
 
   it('should throw an error when read multi-document without option', () => {
     return readFiles('./yaml-samples/multi-docs')
       .then(() => expect.fail('should not come to here'))
-      .catch(error => {
+      .catch((error) => {
         expect(error.name).not.to.be.eql('AssertionError');
         expect(error.name).to.be.eql('YAMLException');
       });
   });
 
   it('should read multi-document with option', () => {
-    return readFiles('./yaml-samples/multi-docs', {multi: true})
-      .then(result => {
+    return readFiles('./yaml-samples/multi-docs', { multi: true }).then(
+      (result) => {
         expect(result).to.be.an('array').to.have.lengthOf(2);
         expect(result).to.be.eql([
           [
@@ -78,58 +78,60 @@ describe('node-read-yaml-files', () => {
             }
           ]
         ]);
-      });
+      }
+    );
   });
 
   it('should flatten result', () => {
-    return readFiles('./yaml-samples/multi-docs', {multi: true, flatten: true})
-      .then(result => {
-        expect(result).to.be.an('array').to.have.lengthOf(4);
-        expect(result).to.be.eql([
-          {
-            bar: 'foo'
-          },
-          {
-            bee: 'boo'
-          },
-          {
-            foo: 'bar'
-          },
-          {
-            far: 'for'
-          }
-        ]);
-      });
+    return readFiles('./yaml-samples/multi-docs', {
+      multi: true,
+      flatten: true
+    }).then((result) => {
+      expect(result).to.be.an('array').to.have.lengthOf(4);
+      expect(result).to.be.eql([
+        {
+          bar: 'foo'
+        },
+        {
+          bee: 'boo'
+        },
+        {
+          foo: 'bar'
+        },
+        {
+          far: 'for'
+        }
+      ]);
+    });
   });
 
   it('should return a 2-depth array', () => {
-    return readFiles('./yaml-samples/array')
-      .then(result => {
-        expect(result).to.be.an('array').to.have.lengthOf(2);
-        expect(result).to.be.eql([
-          [
-            {
-              bar: 'foo'
-            },
-            {
-              bee: 'boo'
-            }
-          ],
-          [
-            {
-              foo: 'bar'
-            },
-            {
-              far: 'for'
-            }
-          ]
-        ]);
-      });
+    return readFiles('./yaml-samples/array').then((result) => {
+      expect(result).to.be.an('array').to.have.lengthOf(2);
+      expect(result).to.be.eql([
+        [
+          {
+            bar: 'foo'
+          },
+          {
+            bee: 'boo'
+          }
+        ],
+        [
+          {
+            foo: 'bar'
+          },
+          {
+            far: 'for'
+          }
+        ]
+      ]);
+    });
   });
 
   it('should return a 1-depth array with flatten option', () => {
-    return readFiles('./yaml-samples/array', {flatten: true})
-      .then(result => {
+    return readFiles('./yaml-samples/array', { flatten: true }).then(
+      (result) => {
         expect(result).to.be.an('array').to.have.lengthOf(4);
         expect(result).to.be.eql([
           {
@@ -145,64 +147,68 @@ describe('node-read-yaml-files', () => {
             far: 'for'
           }
         ]);
-      });
+      }
+    );
   });
 
   it('should return a 2-depth array with flatten option', () => {
-    return readFiles('./yaml-samples/array-2-depth', {flatten: true})
-      .then(result => {
+    return readFiles('./yaml-samples/array-2-depth', { flatten: true }).then(
+      (result) => {
         expect(result).to.be.an('array').to.have.lengthOf(4);
         expect(result).to.be.eql([
-          [{a1: 1}, {a2: 2}],
-          [{b1: 3}, {b2: 4}],
-          [{c1: 5}, {c2: 6}],
-          [{d1: 7}, {d2: 8}]
+          [{ a1: 1 }, { a2: 2 }],
+          [{ b1: 3 }, { b2: 4 }],
+          [{ c1: 5 }, { c2: 6 }],
+          [{ d1: 7 }, { d2: 8 }]
         ]);
-      });
+      }
+    );
   });
 
   it('should return a 1-depth array with flatten option and depth option', () => {
-    return readFiles('./yaml-samples/array-2-depth', {flatten: true, depth: 2})
-      .then(result => {
-        expect(result).to.be.an('array').to.have.lengthOf(8);
-        expect(result).to.be.eql([
-          {a1: 1},
-          {a2: 2},
-          {b1: 3},
-          {b2: 4},
-          {c1: 5},
-          {c2: 6},
-          {d1: 7},
-          {d2: 8}
-        ]);
-      });
+    return readFiles('./yaml-samples/array-2-depth', {
+      flatten: true,
+      depth: 2
+    }).then((result) => {
+      expect(result).to.be.an('array').to.have.lengthOf(8);
+      expect(result).to.be.eql([
+        { a1: 1 },
+        { a2: 2 },
+        { b1: 3 },
+        { b2: 4 },
+        { c1: 5 },
+        { c2: 6 },
+        { d1: 7 },
+        { d2: 8 }
+      ]);
+    });
   });
 
   it('should return a 3-depth array', () => {
-    return readFiles('./yaml-samples/array-2-depth')
-      .then(result => {
-        expect(result).to.be.an('array').to.have.lengthOf(2);
-        expect(result).to.be.eql([
-          [
-            [{a1: 1}, {a2: 2}],
-            [{b1: 3}, {b2: 4}]
-          ],
-          [
-            [{c1: 5}, {c2: 6}],
-            [{d1: 7}, {d2: 8}]
-          ]
-        ]);
-      });
+    return readFiles('./yaml-samples/array-2-depth').then((result) => {
+      expect(result).to.be.an('array').to.have.lengthOf(2);
+      expect(result).to.be.eql([
+        [
+          [{ a1: 1 }, { a2: 2 }],
+          [{ b1: 3 }, { b2: 4 }]
+        ],
+        [
+          [{ c1: 5 }, { c2: 6 }],
+          [{ d1: 7 }, { d2: 8 }]
+        ]
+      ]);
+    });
   });
 
   it('should return an array with file path', () => {
-    return readFiles('./yaml-samples/simple', {withFile: true})
-      .then(result => {
+    return readFiles('./yaml-samples/simple', { withFile: true }).then(
+      (result) => {
         expect(result).to.be.an('array').to.have.lengthOf(2);
         expect(result).to.be.eql([
-          {file: 'yaml-samples/simple/bar.yml', data: {bar: 'foo'}},
-          {file: 'yaml-samples/simple/foo.yml', data: {foo: 'bar'}}
+          { file: 'yaml-samples/simple/bar.yml', data: { bar: 'foo' } },
+          { file: 'yaml-samples/simple/foo.yml', data: { foo: 'bar' } }
         ]);
-      });
+      }
+    );
   });
 });
